2c5e66ff0a8daa022aceb7aa7690b79f02359096
Enzo Kummer <enzokummer@gmail.com>
2024-09-01 23:53:17 -0300

    FINAL

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys, os
+import numpy as np
+import matplotlib.pyplot as plt
 from PIL import Image
 from AES import AES
+
 
 def process_text_file(file_path):
     with open(file_path, 'rb') as file:
@@ -41,6 +44,33 @@ def save_decrypted_data(decrypted_data, file_path, width=None, height=None):
                 if encoding == encodings[-1]:
                     print(f"O arquivo descriptografado não pôde ser lido como texto usando as codificações: {', '.join(encodings)}")
 
+def render_encrypted_image(encrypted_file_path, output_image_path, width, height):
+    # Ler os dados criptografados
+    with open(encrypted_file_path, 'rb') as file:
+        encrypted_data = file.read()
+
+    # Converte os bytes em um array NumPy
+    data_array = np.frombuffer(encrypted_data, dtype=np.uint8)
+
+    # Reshape o array para as dimensões da imagem
+    # Se o arquivo criptografado não tiver exatamente o tamanho esperado, isso pode causar problemas
+    try:
+        image_array = data_array[:width*height*3].reshape((height, width, 3))
+    except ValueError:
+        print("Erro: Os dados não correspondem às dimensões fornecidas.")
+        return
+
+    # Cria uma imagem a partir do array
+    img = Image.fromarray(image_array)
+
+    # Salva a imagem
+    img.save(output_image_path)
+
+    # Exibe a imagem
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
+
 def main():
     if len(sys.argv) != 3:
         print("Uso correto: python main.py <caminho_para_arquivo> <numero de rodadas>")
@@ -63,14 +93,19 @@ def main():
 
         decrypted_data = aes.decrypt_ctr(encrypted_data, iv)
         save_decrypted_data(decrypted_data, file_path, width, height)
+        dir_name, base_name = os.path.split(file_path)
+
+        path_imagem_criptografada = os.path.join(dir_name, f'encrypted_{base_name}')
+        path_imagem_renderizada = 'criptografada_renderizada.png'
+        render_encrypted_image(path_imagem_criptografada, path_imagem_renderizada, width, height)
 
         print(f"Imagem criptografada salva como 'encrypted_{file_path}'")
+        print("Renderizamos a imagem criptografada e a salvamos.")
         print(f"Imagem descriptografada salva como 'decrypted_{file_path}'")
             
     else:
         data = process_text_file(file_path)
         print(f"Texto original: {data}")
-        print(f"Tamanho dos dados de entrada: {len(data)}")
 
         key = b'\x2b\x7e\x15\x16\x28\xae\xd2\xa6\xab\xf7\xcf\x4f\x3c\x1e\xfb\x15'
         iv = os.urandom(16)
@@ -79,15 +114,11 @@ def main():
         aes = AES(key, num_rounds)
 
         encrypted_data = aes.encrypt_ctr(data, iv)
-        print(f"Dados criptografados: {encrypted_data.hex()}")
-        print(f"Tamanho dos dados criptografados: {len(encrypted_data)} bytes")
-
+       
         save_encrypted_data(encrypted_data, file_path)
 
         decrypted_data = aes.decrypt_ctr(encrypted_data, iv)
-        print(f"Dados descriptografados: {decrypted_data}")
-        print(f"Tamanho dos dados descriptografados: {len(decrypted_data)} bytes")
-
+        
         save_decrypted_data(decrypted_data, file_path)
 
         print(f"Arquivo criptografado salvo como 'encrypted_{file_path}'")
